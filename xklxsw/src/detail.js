de38fafd3c120e7ec5efadc05fe1655057869395
hishirooo <nguyenhuuthienson@gmail.com>
2021-09-03 17:48:24 +0700

    thay đổi decription

--- xklxsw/src/detail.js
+++ xklxsw/src/detail.js
@@ -7,8 +7,7 @@ function execute(url) {
             host: "https://www.xklxsw.com/",
             author: doc.select(".content .fd-list").text().match(/作者：(.+?)\s字数/)[1],
             //author: doc.select(".content .fd-list h1").text(),
-            description: doc.select(".content .fd-list").text().match(/简介：(.*?)。 《/)[1],
-            status: doc.select(".content .fd-list").text().match(/状态：(.+?) 简介/)[1]
+            description: doc.select(".content .fd-list").text().match(/简介：(.*?)。 《/)[1]+"\n"+ doc.select(".content .fd-list").text().match(/状态：(.+?) 简介/)[1],
         });
     }
     return null;
