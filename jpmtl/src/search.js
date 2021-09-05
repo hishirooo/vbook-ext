07ecf7e504303746679842f3f11fb9cf44c0927f
hishirooo <nguyenhuuthienson@gmail.com>
2021-09-05 14:14:14 +0700

    add search

--- jpmtl/src/search.js
+++ jpmtl/src/search.js
@@ -1,2 +1,22 @@
 function execute(key, page) {
+    if (!page) page = '1';
+    var doc = Http.get("https://jpmtl.com/v2/book/show/browse?query="+key+"&categories&content_type=0&direction=0&limit=20&type=5&status=all&language=3&exclude_categories&page=" + page).string();
+    var json_search = JSON.parse(doc);
+    var book_search = [];
+    for(var i in json_search){
+        var book = json_search[i];
+        book_search.push({
+            name: book.title,
+            link: '/books/' + book.id,
+            cover: book.cover,
+            description: book.author,
+            host:"https://jpmtl.com"
+
+        });
+
+    }
+    var next  = parseInt(page) + 1
+//name , link,  cover, description, host
+// push
+    return Response.success(book_search, next.toString());
 }
