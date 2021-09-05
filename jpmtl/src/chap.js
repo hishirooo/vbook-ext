5b2b31fc34971d79286445f6fbfddb5ab971f56a
hishirooo <nguyenhuuthienson@gmail.com>
2021-09-05 11:28:55 +0700

    fix view content chapter

--- jpmtl/src/chap.js
+++ jpmtl/src/chap.js
@@ -1,11 +1,10 @@
 function execute(url) {
     var doc = Http.get(url).html();
     if (doc){
-        var content = doc.select(".cp-content").html()
-        var text_remove = "<p> This novel has been translated by JPMTL.com and if you are reading this somewhere, they have stolen our translation. <\/p>"
-        while(content.search(text_remove))
-            content =  content.replace(text_remove,"")  
+        var content = doc.select(".cp-content").html();
+        var text_remove = "<p> This novel has been translated by JPMTL.com and if you are reading this somewhere, they have stolen our translation. <\/p>";
+        content = content.replaceAll(text_remove,"");
         return Response.success(content);
     }   
-     return null;
+    return null;
 }
