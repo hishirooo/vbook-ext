c884a5452a0befba6a451c9cc4fd9b671bb8f979
hishirooo <nguyenhuuthienson@gmail.com>
2021-09-05 11:50:44 +0700

    fix chapter

--- jpmtl/src/chap.js
+++ jpmtl/src/chap.js
@@ -1,13 +1,11 @@
 function execute(url) {
-<<<<<<< HEAD
     var doc = Http.get(url).html();
     if (doc){
         var content = doc.select(".cp-content").html();
-        var text_remove = "<p> This novel has been translated by JPMTL.com and if you are reading this somewhere, they have stolen our translation. <\/p>";
-        content = content.replace(text_remove,"").replace(text_remove,"");
+        content =  content.replace("<p> This novel has been translated by JPMTL.com and if you are reading this somewhere, they have stolen our translation. <\/p>","")
+        content = content.replace("<p> This novel has been translated by JPMTL.com and if you are reading this somewhere, they have stolen our translation. <\/p>","")
+        
         return Response.success(content);
     }   
     return null;
-=======
->>>>>>> parent of 44a23e7 (add Chapter)
 }
