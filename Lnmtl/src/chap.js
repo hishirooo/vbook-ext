6ffa070eefbfe60fafea26204303ec5e54f8e683
hishirooo <nguyenhuuthienson@gmail.com>
2021-09-07 20:38:08 +0700

    fix reg chapter

--- Lnmtl/src/chap.js
+++ Lnmtl/src/chap.js
@@ -3,7 +3,7 @@ function execute(url) {
     var doc = Http.get(url).html();
     if (doc){
         //var content = doc.select(".container div div.chapter-body sentence.translated").toString().replace(/<w/g,"<t").replace(/w>/g,"t>");
-        var content = doc.select(".container div div.chapter-body sentence.translated").text().replace(/\.(\s|\u)/g,'\.<br>')
+        var content = doc.select(".container div div.chapter-body sentence.translated").text().replace(/\.(\s|\u|)/g,'\.<br>')
         //var text = content.replace(/<[^>\n]*>/g,"")
         return Response.success(content);
     }   
