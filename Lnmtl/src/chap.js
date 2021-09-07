5882d8f98e120cdb2c1ef65bff042e96f95c1be4
hishirooo <nguyenhuuthienson@gmail.com>
2021-09-07 21:20:52 +0700

    text code

--- Lnmtl/src/chap.js
+++ Lnmtl/src/chap.js
@@ -2,10 +2,10 @@ function execute(url) {
 
     var doc = Http.get(url).html();
     if (doc){
-        //var content = doc.select(".container div div.chapter-body sentence.translated").toString().replace(/<w/g,"<t").replace(/w>/g,"t>");
-        //var content = doc.select(".container div div.chapter-body sentence.translated").text().replace(/\.(\s|\u|)/g,'\.<br>')
         var content = doc.select(".container div div.chapter-body sentence.translated").html()
-        //var text = content.replace(/<[^>\n]*>/g,"")
+            .replace(/<\/?w.*?>/g, "")
+            .replace(/<\/?t.*?>/g, "")
+            .replace(/[\n]/g, "<br>")
         return Response.success(content);
     }   
     return null;
