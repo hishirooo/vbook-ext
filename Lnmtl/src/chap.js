bac7f047b054c07e64ddaf6f1322c7d59f6fd8cb
hishirooo <nguyenhuuthienson@gmail.com>
2021-09-07 20:24:52 +0700

    Chia đoạn text chapter

--- Lnmtl/src/chap.js
+++ Lnmtl/src/chap.js
@@ -1,11 +1,11 @@
 function execute(url) {
-//books/
+
     var doc = Http.get(url).html();
     if (doc){
         //var content = doc.select(".container div div.chapter-body sentence.translated").toString().replace(/<w/g,"<t").replace(/w>/g,"t>");
-        var content = doc.select(".container div div.chapter-body sentence.translated").toString()
-        var text = content.replace(/<[^>\n]*>/g,"")
-        return Response.success(text);
+        var content = doc.select(".container div div.chapter-body sentence.translated").text().replace(/\.(\s|\u)/g,'\.\n')
+        //var text = content.replace(/<[^>\n]*>/g,"")
+        return Response.success(content);
     }   
     return null;
 }
