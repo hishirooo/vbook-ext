c85ef20adeed74e616fe0458c3bfbac7e1571533
hishirooo <nguyenhuuthienson@gmail.com>
2021-09-12 15:25:29 +0700

    ad chapter

--- yushuwuy/src/chap.js
+++ yushuwuy/src/chap.js
@@ -1,2 +1,10 @@
 function execute(url) {
+//books/
+    var doc = Http.get(url).html();
+    if (doc){
+        var content = doc.select("div#nr div#nr1 p");
+
+        return Response.success(content);
+    }   
+    return null;
 }
