42e9ad7da3ee500870ffd777c921e4739f232edd
hishirooo <nguyenhuuthienson@gmail.com>
2021-09-26 11:27:55 +0700

    fix chapter

--- yushuwuy/src/chap.js
+++ yushuwuy/src/chap.js
@@ -1,10 +1,10 @@
-function execute(url) {
+function execute(url,page) {
 //books/
+    var content = []
     var doc = Http.get(url).html();
-    if (doc){
-        var content = doc.select("div#nr div#nr1 p");
-
-        return Response.success(content);
-    }   
-    return null;
+    var checkpage = doc.select("td.next a").attr("href").match(/(\d+).html/)[1]
+    if(parseInt(checkpage)>0) 
+        Console.log(checkpage)
+    content = content + doc.select("div#nr div#nr1 p");
+    return Response.success(content,page);
 }
