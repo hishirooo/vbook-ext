575208c295efd53025024e4a7d165b1da0b9c69e
hishirooo <nguyenhuuthienson@gmail.com>
2021-10-04 23:23:32 +0700

    creat genhome, fix view detail

--- tusachxinhxinh/src/detail.js
+++ tusachxinhxinh/src/detail.js
@@ -1,12 +1,28 @@
 function execute(url) {
     var doc = Http.get(url + "/").html()
+    //----------------
+    var ratingCount = doc.select(".vote-details > span")[1].text() + "<br>"
+
+    var otherName =  doc.select(".post-content_item")[2].text() + "<br>"
+    if(otherName.indexOf('Other name: ')==-1)
+        otherName = ""
+    var content = doc.select(".site-content > div > div")[1].select(".summary__content").text()
+ 
+    var rank = doc.select(".post-content_item")[1].text() + "<br>"
+
+    var  author = doc.select(".author-content a").text()
+    if(author=="") author ="Updating" + "<br>"
+
+    var ongoing = doc.select(".post-status").text()
+    //----------------
+    
     return Response.success({
         name : doc.select(".post-title h1").text(),
         cover : doc.select(".summary_image img").attr("src"),
         host : "https://tusachxinhxinh.com",
-        author : doc.select(".comic-intro-text span")[1].text(),
-        // description : doc.select(".text-justify p").text(),
-        // ongoing : ongoing.indexOf('Đang tiến hành')!=-1,
-        detail :"",
+        author : author,
+        description : content,
+        ongoing : ongoing.indexOf('Đang tiến hành')!=-1,
+        detail :"Rate: " + ratingCount + rank + otherName + "Author: " + author + "<br>Status: " + ongoing
     })
 }
---
+++ tusachxinhxinh/src/genhome.js
@@ -0,0 +1,66 @@
+function execute(url, page) {
+    //var genre = url.match(/the-loai.(.+)\//)[1]
+
+    // khai bao params theo url-----------
+    var orderby = "meta_value_num"
+    var order = "desc"
+    var meta_key = ""
+    if(url=='latest'){
+        meta_key = "_latest_update"
+    }
+    else{
+        if(url=='trending'){
+            meta_key = "_wp_manga_week_views_value"
+        }
+        else{
+            if(url=='a-z'){
+                orderby = "post_title"
+                order = "ASC"
+                meta_key = ""
+            }
+            else{
+                if(url=='mostview'){
+                    meta_key = "_wp_manga_views"
+                    order = ""
+                }
+                else{
+                    orderby = "date"
+                    meta_key =""
+                    order =""
+                }
+            }
+        }
+    }
+    //------------
+    if(!page) page="0"
+    var doc = Http.post("https://tusachxinhxinh.com/wp-admin/admin-ajax.php")
+    .params({
+        "action": "madara_load_more",
+        "page": page,
+        "template": "madara-core/content/content-search",
+        "vars[orderby]": orderby,
+        "vars[paged]": "1",
+        "vars[template]": "search",
+        "vars[meta_query][0][relation]": "AND",
+        "vars[meta_query][relation]": "OR",
+        "vars[post_type]": "wp-manga",
+        "vars[post_status]": "publish",
+        "vars[meta_key]": meta_key,
+        "vars[order]": order,
+        "vars[manga_archives_item_layout]": "default"
+    }).html();
+    var listBook = []
+    var books = doc.select(".c-tabs-item__content .c-image-hover")
+    books.forEach(function(book){
+        listBook.push({
+            name: book.select("a").attr("title"),
+            link: book.select("a").attr("href"),
+            cover: book.select("img").attr("src"),
+            description: "",
+            host: "https://tusachxinhxinh.com"    
+        })
+    })
+    if (listBook.length == 0) next = ""; 
+    else next = (parseInt(page) + 1).toString();
+    return Response.success(listBook,next)
+}
