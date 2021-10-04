dcedfdf2b25307d5234bf016423a3e6010def5d3
hishirooo <nguyenhuuthienson@gmail.com>
2021-10-04 15:10:20 +0700

    Update gen.js

--- tusachxinhxinh/src/gen.js
+++ tusachxinhxinh/src/gen.js
@@ -1,58 +1,46 @@
 function execute(url, page) {
-    var url = "https://tusachxinhxinh.com/the-loai/action/"
     var genre = url.match(/the-loai.(.+)\//)[1]
     if(!page) page="0"
-    var request = Http.get(url)
     var doc = Http.post("https://tusachxinhxinh.com/wp-admin/admin-ajax.php")
-  .headers({
-    "authority": "tusachxinhxinh.com",
-    "accept": "*/*",
-    "sec-fetch-dest": "empty",
-    "x-requested-with": "XMLHttpRequest",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Whale/2.7.100.20 Safari/537.36",
-    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "origin": "https://tusachxinhxinh.com",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-mode": "cors",
-    "referer": url,
-    "accept-language": "en-US,en;q=0.9,ko;q=0.8,vi;q=0.7",
-    "cookie": request.cookie(),
-    "Authorization": "Bearer null"
-  })
-  .params({
-    "action": "madara_load_more",
-    "page": page,
-    "template": "madara-core%2Fcontent%2Fcontent-archive",
-    "vars%5Bwp-manga-genre%5D": genre,
-    "vars%5Bp%5D": "0",
-    "vars%5Battachment_id%5D": "0",
-    "vars%5Bpage_id%5D": "0",
-    "vars%5Bday%5D": "0",
-    "vars%5Bmonthnum%5D": "0",
-    "vars%5Byear%5D": "0",
-    "vars%5Bw%5D": "0",
-    "vars%5Bpaged%5D": "1",
-    "vars%5Bmeta_key%5D": "_latest_update",
-    "vars%5Bignore_sticky_posts%5D": "false",
-    "vars%5Bsuppress_filters%5D": "false",
-    "vars%5Bcache_results%5D": "false",
-    "vars%5Bupdate_post_term_cache%5D": "true",
-    "vars%5Blazy_load_term_meta%5D": "true",
-    "vars%5Bupdate_post_meta_cache%5D": "true",
-    "vars%5Bpost_type%5D": "wp-manga",
-    "vars%5Bposts_per_page%5D": "12",
-    "vars%5Bnopaging%5D": "false",
-    "vars%5Bcomments_per_page%5D": "50",
-    "vars%5Bno_found_rows%5D": "false",
-    "vars%5Btaxonomy%5D": "wp-manga-genre",
-    "vars%5Bterm%5D": "comedy",
-    "vars%5Border%5D": "desc",
-    "vars%5Borderby%5D": "meta_value_num",
-    "vars%5Btemplate%5D": "archive",
-    "vars%5Bsidebar%5D": "right",
-    "vars%5Bpost_status%5D": "publish",
-    "vars%5Bmeta_query%5D%5Brelation%5D": "OR"
-  }).html();
-
-  return Response.success(doc)
+    .params({
+        "action": "madara_load_more",
+        "page": page,
+        "template": "madara-core/content/content-archive",
+        "vars[wp-manga-genre]": genre,
+        "vars[paged]": "1",
+        "vars[meta_key]": "_latest_update",
+        "vars[ignore_sticky_posts]": false,
+        "vars[suppress_filters]": false,
+        "vars[cache_results]": false,
+        "vars[update_post_term_cache]": true,
+        "vars[lazy_load_term_meta]": true,
+        "vars[update_post_meta_cache]": true,
+        "vars[post_type]": "wp-manga",
+        "vars[posts_per_page]": 12,
+        "vars[nopaging]": false,
+        "vars[comments_per_page]": 50,
+        "vars[no_found_rows]": false,
+        "vars[taxonomy]": "wp-manga-genre",
+        "vars[term]": genre,
+        "vars[order]": "desc",
+        "vars[orderby]": "meta_value_num",
+        "vars[template]": "archive",
+        "vars[sidebar]": "right",
+        "vars[post_status]": "publish",
+        "vars[meta_query][relation]": "OR",
+    }).html();
+    var listBook = []
+    var books = doc.select(".page-item-detail .c-image-hover")
+    books.forEach(function(book){
+        listBook.push({
+            name: book.select("a").attr("title"),
+            link: book.select("a").attr("href"),
+            cover: book.select("img").attr("src"),
+            description: "",
+            host: "https://tusachxinhxinh.com"    
+        })
+    })
+    if (listBook.length == 0) next = ""; 
+    else next = (parseInt(page) + 1).toString();
+  return Response.success(listBook,next)
 }
