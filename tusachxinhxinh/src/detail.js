92bf672393bfc3c5df4bfc9f4482dfda3cdbe956
hishirooo <nguyenhuuthienson@gmail.com>
2021-10-04 18:08:06 +0700

    test detail tusach

--- tusachxinhxinh/src/detail.js
+++ tusachxinhxinh/src/detail.js
@@ -1,2 +1,12 @@
 function execute(url) {
+    var doc = Http.get(url + "/").html()
+    return Response.success({
+        name : doc.select(".post-title h1").text(),
+        cover : doc.select(".summary_image img").attr("src"),
+        host : "https://tusachxinhxinh.com",
+        author : doc.select(".comic-intro-text span")[1].text(),
+        // description : doc.select(".text-justify p").text(),
+        // ongoing : ongoing.indexOf('Đang tiến hành')!=-1,
+        detail :"",
+    })
 }
