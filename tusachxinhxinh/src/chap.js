24136bd5ca0da9a8fcfef24fe045a251a9b06abf
hishirooo <nguyenhuuthienson@gmail.com>
2021-10-05 18:23:38 +0700

    update code chapter

--- tusachxinhxinh/src/chap.js
+++ tusachxinhxinh/src/chap.js
@@ -1,2 +1,9 @@
 function execute(url) {
+    var doc = Http.get(url).html()
+    var images = doc.select(".wp-manga-chapter-img")
+    var listImage = []
+    images.forEach(function(image){
+        listImage.push(image.attr("src").replace(/\t|\n/g,''))
+    })
+    return Response.success(listImage)
 }
