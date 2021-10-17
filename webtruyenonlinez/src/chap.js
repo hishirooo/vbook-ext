9779d34ee884eca93c87bddc68fb642c714c068c
hishirooo <nguyenhuuthienson@gmail.com>
2021-10-17 23:02:44 +0700

    add truyenkk

--- webtruyenonlinez/src/chap.js
+++ webtruyenonlinez/src/chap.js
@@ -2,5 +2,5 @@ function execute(url) {
     var doc = Http.get(url + "/").html()
     var content = doc.select("#contents > div")[1].html()
     content = content.replace(/(<br>\s*){2,}/gm, '<br>');
-    return Response.success(content)
+    return Response.success(doc)
 }
