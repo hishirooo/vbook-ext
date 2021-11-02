751121bb1f8a47d86cd3c0023dbb154b4d4f8233
hishirooo <nguyenhuuthienson@gmail.com>
2021-11-02 21:14:28 +0700

    final

--- sxyxht/src/chap.js
+++ sxyxht/src/chap.js
@@ -1,38 +1,32 @@
 function execute(url) {
-    //https://m.sxyxht.com/dushu/12350_150_1.html
-    var page = "0"
-    var numPage = 0
-    var newUrl = url.match(/(.+)(.html)/)
+    var doc = Http.get(url).string()
+    var article_id = doc.match(/article_id = \"(\d+)\"/)[1]
+	var chapter_id = doc.match(/chapter_id = \"(\d+)\"/)[1]
+	var hash = doc.match(/hash = \"(.+)\"/)[1]
+    var json = Http.post('https://sxyxht.com/home/index/ajaxchapter').params({
+        "id": article_id,
+        "eKey": hash,
+        "cid": chapter_id
+    }).string()
+    var content = JSON.parse(json).info.content;
 
-
-    var doc = Http.get(newUrl[1] + "_" + page + ".html").html()
-
-    var text = ""
-    var checkNext = doc.select(".content .content_btn")[1].select(".ptm-col-xs-3")[1].select("a").attr("href").split("_").length
-
-    while(numPage==0||checkNext!=2){
-        doc = Http.get(newUrl[1] + "_" + page + ".html").html()
-        
-        doc.select('p').remove()
-        doc.select('script').remove()
-        doc.select('ins').remove()
-        
-        text2 =doc.select("#BookText").html()
-
-        if(text2.replace(/<![^>]+>/g,'')!=""){
-            //Console.log(text2)
-            text2 = text2.replace(/<br><br>/g,'NHTS')
-            text2 = text2.replace(/<[^>]+>/g,'')
-            text2 = text2.replace(/&nbsp;/g,'')
-            text2 = text2.replace(/--&gt;&gt;/g,'')
-            text2 = text2.replace(/NHTS/g,'<br>')
-            text = text + text2
-            page = (parseInt(page) + 1).toString()
-            numPage +=1
-            checkNext = doc.select(".content .content_btn")[1].select(".ptm-col-xs-3")[1].select("a").attr("href").split("_").length
-
-        }
-
+    //phân trang 
+    var nextPage = (Html.parse(doc)).select(".link a[rel='next']");
+    while(nextPage.text() === '下一页'){
+        var doc2 = Http.get('https://sxyxht.com/'+nextPage.attr('href')).string();
+        var article_id2 = doc2.match(/article_id = \"(\d+)\"/)[1]
+        var chapter_id2 = doc2.match(/chapter_id = \"(\d+)\"/)[1]
+        var hash2 = doc2.match(/hash = \"(.+)\"/)[1]
+        var json2 = Http.post('https://sxyxht.com/home/index/ajaxchapter').params({
+            "id": article_id2,
+            "eKey": hash2,
+            "cid": chapter_id2
+        }).string()
+        var content2 = JSON.parse(json2).info.content;
+        content = content + content2
+        var nextPage = (Html.parse(doc2)).select(".link a[rel='next']");
     }
-    return Response.success(text)
+    //rac
+    content = content.replace(/(<br>\s*){2,}/g,'<br>')
+    return Response.success(content)
 }
