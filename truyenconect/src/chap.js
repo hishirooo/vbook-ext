22aa09190c295139bd48afb873953f3aab7313e6
hishirooo <nguyenhuuthienson@gmail.com>
2021-11-05 20:37:31 +0700

    fix load chương

--- truyenconect/src/chap.js
+++ truyenconect/src/chap.js
@@ -1,5 +1,13 @@
 function execute(url) {
     var doc = Http.get(url).html()
-    var text = doc.select(".reading-content p")
+    var text = doc.select(".reading-content .text-left")
+    text.select("h2").remove()
+    text.select("script").remove()
+    text.select("ins").remove()
+    text = text.html()
+
+    text = text.replace(/&lt;br&gt;/g,'<br>')
+    text = text.replace(/(<br>){2,}/g,'<br>')
+    text = text.replace(/(<br>\s*){2,}/g,'<br>')
     return Response.success(text)
 }
