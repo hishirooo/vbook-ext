ce81404be6c23edd2cf5d9e96ae107a80f8da543
hishirooo <nguyenhuuthienson@gmail.com>
2021-12-18 21:23:25 +0700

    fix code allpormcomic

--- allporncomic/src/genre.js
+++ allporncomic/src/genre.js
@@ -1,6 +1,8 @@
 function execute() {
     var doc = Http.get("https://allporncomic.com/home-3/").html()
-    var genres = doc.select(".sub-nav_list li")[1].select(".sub-menu li a")
+    
+    var genres = doc.select(".sub-nav_list li").get(1).select(".sub-menu li a")
+    //return Response.success(genres)
     var listGenre = []
     genres.forEach(genre => listGenre.push({
         title : genre.select("span").text(),
