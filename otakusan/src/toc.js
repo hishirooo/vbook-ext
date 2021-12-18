df7da50499110afe642da3004765d65426b82ece
hishirooo <nguyenhuuthienson@gmail.com>
2021-12-18 20:24:22 +0700

    fix toc

--- otakusan/src/toc.js
+++ otakusan/src/toc.js
@@ -1,11 +1,15 @@
 function execute(url) {
     var doc = fetch(url).html();
-
-    let listChapter = doc.select(".chapter-list tbody > .chapter").map(chapter => ({
-        name: chapter.select(".read-chapter").get(1).select("a").attr("alt"),
-        url: chapter.select(".read-chapter").get(1).select("a").attr("href"),
-        host: "https://otakusan.net"
-    }))
+    var chapters = doc.select(".chapter-list tbody > .chapter")
+    var listChapter = []
+    for( var i=chapters.size()-1; i>=0 ;i--){
+        var ct = chapters.get(i).select(".read-chapter")
+        listChapter.push({
+            name: ct.select(".read-chapter").get(1).select("a").attr("alt"),
+            url: ct.select(".read-chapter").get(1).select("a").attr("href"),
+            host: "https://otakusan.net"
+        })
+    }
     return Response.success(listChapter);
 }
 //https://otakusan.net/chapter/1268234/voi-himeno-chan-thi-yeu-con-la-qua-som-do-chap-34
