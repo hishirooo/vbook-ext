3931849b3c3c5be5ee20e3b4dfadcaa7f728356d
hishirooo <nguyenhuuthienson@gmail.com>
2021-12-19 20:19:51 +0700

    fix code

--- webtruyenonlinez/src/chap.js
+++ webtruyenonlinez/src/chap.js
@@ -1,6 +1,6 @@
 function execute(url) {
     var doc = Http.get(url + "/").html()
-    var content = doc.select("#contents > div")[1].html()
+    var content = doc.select("#contents > div").get(1).html()
     content = content.replace(/(<br>\s*){2,}/gm, '<br>');
-    return Response.success(doc)
+    return Response.success(content)
 }
--- webtruyenonlinez/src/genre.js
+++ webtruyenonlinez/src/genre.js
@@ -1,6 +1,6 @@
 function execute() {
     var doc = Http.get("http://webtruyenonlinez.com/").html()
-    var genres = doc.select(".navbar-nav .dropdown-menu")[0].select("li a")
+    var genres = doc.select(".navbar-nav .dropdown-menu").first().select("li a")
     var listGenre = []
     genres.forEach(gen => listGenre.push({
             title: gen.text(),
