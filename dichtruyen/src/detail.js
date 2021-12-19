a2da6d0d2ae47047cb3b2460da7b4e09b9415ffa
hishirooo <nguyenhuuthienson@gmail.com>
2021-12-19 20:15:08 +0700

    fix code

--- dichtruyen/src/detail.js
+++ dichtruyen/src/detail.js
@@ -1,16 +1,16 @@
 function execute(url) {
 	var doc = Http.get(url + "/").html()
 	var infos = doc.select(".lt_infocomic > div")
-	var ongoing = infos[3].select("a").text()
+	var ongoing = infos.get(3).select("a").text()
     var genres = String(doc.select(".comicinfo .lt_cate a")).replace(/<[^>]+>/g,'').replace(/\n/g,', ')
     return Response.success({
         name : doc.select(".comictitle").text(),
         cover : doc.select(".ts_sticker > .img_load div").attr("data-bg"),
         host : "https://dichtruyen.org",
-        author : infos[1].select("span a").text(),
+        author : infos.get(1).select("span a").text(),
         description : doc.select(".lt_info99 > div span").text().replace(/ \.{2,}| Xem thêm/g,""),
         ongoing : ongoing.indexOf('Đang tiến hành')!=-1,
-        detail : "Author: " + infos[1].select("span a").text() + "<br>"
+        detail : "Author: " + infos.get(1).select("span a").text() + "<br>"
         + "Status : " + ongoing + "<br>"
         + "Genre: " + genres
     });
--- dichtruyen/src/genre.js
+++ dichtruyen/src/genre.js
@@ -1,6 +1,6 @@
 function execute() {
 	var doc = Http.get("https://dichtruyen.org/").html()
-	var genres =  doc.select(".dt_headbot_menu .dt_menu_e")[0].select(".dtm_expand a")
+	var genres =  doc.select(".dt_headbot_menu .dt_menu_e").first().select(".dtm_expand a")
 
 	var listGenre = []
 	genres.forEach(genre =>listGenre.push({
