9366e653875b26bbe4a4e06eb6e23b3d32f6a60d
Cá Mụp Cắn Cáp <nguyenhuuthienson@gmail.com>
2022-04-28 20:08:04 +0700

    update

--- feiszw/src/detail.js
+++ feiszw/src/detail.js
@@ -1,9 +1,10 @@
 function execute(url) {
+    url = url.replace("m.feiszw","feiszw")
     var doc = fetch(url).html()
     
     return Response.success({
         name: doc.select(".info p").get(0).select("a").text(),
-        cover: "https://www.feiszw.com/" + doc.select(".bookimg a img").attr("src"),
+        cover: doc.select(".bookimg a img").attr("src"),
         author: doc.select(".info p").get(0).select("span").text(),
         description: doc.select(".intro").text(),
         detail: "",
