c1eb4be739350ca3726ca7c82baf08789a8e5172
Đường đen <79037079+duongden@users.noreply.github.com>
2022-12-25 11:42:21 +0700

    fix 256wxc
    fix 256wxc

--- 256wxc/src/home.js
+++ 256wxc/src/home.js
@@ -1,13 +1,13 @@
 function execute() {
     return Response.success([
 
-        { title: "现代耽美", input: "https://www.256wenku.com/xiandai/index", script: "gen.js" },
-        { title: "古代架空", input: "https://www.256wenku.com/jiakong/index", script: "gen.js" },
-        { title: "穿越重生", input: "https://www.256wenku.com/chuanyue/index", script: "gen.js" },
-        { title: "BL同人", input: "https://www.256wenku.com/bltongren/index", script: "gen.js" },
-        { title: "GL百合", input: "https://www.256wenku.com/glbaihe/index", script: "gen.js" }, 
-        { title: "玄幻科幻", input: "https://www.256wenku.com/xuanhuan/index", script: "gen.js" }, 
-        { title: "都市言情", input: "https://www.256wenku.com/yanqing/index", script: "gen.js" }, 
-        { title: "都市言情", input: "https://www.256wenku.com/gudaiyanqing/index", script: "gen.js" }, 
+        { title: "现代耽美", input: "https://www.256wenku.com/xiandai/", script: "gen.js" },
+        { title: "古代架空", input: "https://www.256wenku.com/jiakong/", script: "gen.js" },
+        { title: "穿越重生", input: "https://www.256wenku.com/chuanyue/", script: "gen.js" },
+        { title: "BL同人", input: "https://www.256wenku.com/bltongren/", script: "gen.js" },
+        { title: "GL百合", input: "https://www.256wenku.com/glbaihe/", script: "gen.js" }, 
+        { title: "玄幻科幻", input: "https://www.256wenku.com/xuanhuan/", script: "gen.js" }, 
+        { title: "都市言情", input: "https://www.256wenku.com/yanqing/", script: "gen.js" }, 
+        { title: "都市言情", input: "https://www.256wenku.com/gudaiyanqing/", script: "gen.js" }, 
     ]);
 }
