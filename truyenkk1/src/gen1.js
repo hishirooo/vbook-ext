6cacd0efca095403e1cd316ea9f6caa1b992bab0
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-05-05 16:13:27 +0700

    fix truyenkk

--- truyenkk1/src/gen1.js
+++ truyenkk1/src/gen1.js
@@ -2,16 +2,18 @@ function execute(url, page) {
     if (!page) page = "1"
     var listBook = []
     var newUrl = "https://truyenkkz.com/wp-admin/admin-ajax.php"
-    var action = "load_more_tax"
-    var term_slug = url.match(/keyword\/(.+)/)[1]
+    var action = "rank_bxh_truyen";
+    if(url === "ticket"){
+        action = "load_more_rank";
+    }
     var doc = fetch(newUrl, {
         method: "POST",    // GET, POST, PUT, DELETE, PATCH
         body: {
-            "action": "filter_keyword_tax",
-            "keyword_check": "",
-            "option_keyword_tax": "new-chap",
-            "term[taxonomy]": "keyword",
-            "term[slug]": term_slug,
+            "action": action,
+            "slug": "all",
+            "time": "week",
+            "type": url,
+            "page": page,
         }
     }).html()
     var books = doc.select("h2")
