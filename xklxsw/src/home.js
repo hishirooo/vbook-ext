dfad1da8bb1f99271853f1d63fe61f184de1ba43
Đường đen <79037079+duongden@users.noreply.github.com>
2023-12-08 13:45:14 +0700

    update domain
    update domain

--- xklxsw/src/home.js
+++ xklxsw/src/home.js
@@ -1,14 +1,14 @@
 function execute() {
     return Response.success([
-        { title: "热门小说", input: "https://www.xklxsw.com/hot-allvisit.html", script: "gen.js" },
-        { title: "新书佳作", input: "https://www.xklxsw.com/hot-postdate.html", script: "gen.js" },
-        { title: "玄幻魔法", input: "https://www.xklxsw.com/list-1.html", script: "gen.js" },
-        { title: "仙侠修真", input: "https://www.xklxsw.com/list-2.html", script: "gen.js" },
-        { title: "都市言情", input: "https://www.xklxsw.com/list-3.html", script: "gen.js" },
-        { title: "历史军事", input: "https://www.xklxsw.com/list-4.html", script: "gen.js" },
-        { title: "网游竞技", input: "https://www.xklxsw.com/list-5.html", script: "gen.js" },
-        { title: "科幻小说", input: "https://www.xklxsw.com/list-6.html", script: "gen.js" },
-        { title: "恐怖灵异", input: "https://www.xklxsw.com/list-7.html", script: "gen.js" },
-        { title: "其他小说", input: "https://www.xklxsw.com/list-8.html", script: "gen.js" }
+        { title: "热门小说", input: "https://www.kelexsw.com/hot-allvisit.html", script: "gen.js" },
+        { title: "新书佳作", input: "https://www.kelexsw.com/hot-postdate.html", script: "gen.js" },
+        { title: "玄幻魔法", input: "https://www.kelexsw.com/list-1.html", script: "gen.js" },
+        { title: "仙侠修真", input: "https://www.kelexsw.com/list-2.html", script: "gen.js" },
+        { title: "都市言情", input: "https://www.kelexsw.com/list-3.html", script: "gen.js" },
+        { title: "历史军事", input: "https://www.kelexsw.com/list-4.html", script: "gen.js" },
+        { title: "网游竞技", input: "https://www.kelexsw.com/list-5.html", script: "gen.js" },
+        { title: "科幻小说", input: "https://www.kelexsw.com/list-6.html", script: "gen.js" },
+        { title: "恐怖灵异", input: "https://www.kelexsw.com/list-7.html", script: "gen.js" },
+        { title: "其他小说", input: "https://www.kelexsw.com/list-8.html", script: "gen.js" }
     ]);
 }
--- xklxsw/src/toc.js
+++ xklxsw/src/toc.js
@@ -1,26 +1,27 @@
 //load chương
-function execute(url) {//khi gửi đi nó clear mất /
-    if(url.indexOf('m.xklxsw')!=-1)
-        url = url.replace('m.xklxsw','xklxsw')
-    var doc = Http.get(url+'/').html();
+//kelexsw
+function execute(url) { //khi gửi đi nó clear mất /
+    if (url.indexOf('m.kelexsw') != -1)
+        url = url.replace('m.kelexsw', 'kelexsw')
+    var doc = Http.get(url + '/').html();
     var idtruyen = url.match(/http.*book\/(\d+)/)[1];
     Console.log(idtruyen);
-    if (doc){
+    if (doc) {
         var list = [];
         //var el = doc.select(".content div.fd-list").get(3).select('a')
         var el = doc.select(".content div.fd-list").get(3).select("span.item.chapter a");
-        for (var i = 0; i <= el.size()-1; i++) {
+        for (var i = 0; i <= el.size() - 1; i++) {
             var e = el.get(i);
             list.push({
                 name: e.text(),
-                url: "https://www.xklxsw.com/book/"+idtruyen+"/"+ e.attr("href"),
+                url: "https://www.kelexsw.com/book/" + idtruyen + "/" + e.attr("href"),
                 //url: idtruyen+"/"+ e.attr("href"),
-                host: "https://www.xklxsw.com"
+                host: "https://www.kelexsw.com"
                 //host: "https://www.xklxsw.com/book/"
             });
 
         }
-  
+
     }
     return Response.success(list);
 }
